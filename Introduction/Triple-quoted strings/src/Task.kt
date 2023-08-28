3a46b1dd43065b875bd00846f399b17faa20be80
HYKim <ddh963963@naver.com>
2023-08-28 23:57:17 +0900

    feat : kotlin string template test

--- Introduction/Triple-quoted strings/src/Task.kt
+++ Introduction/Triple-quoted strings/src/Task.kt
@@ -2,8 +2,8 @@ const val question = "life, the universe, and everything"
 const val answer = 42
 
 val tripleQuotedString = """
-    #question = "$question"
-    #answer = $answer""".trimIndent()
+    question = "$question"
+    answer = $answer""".trimIndent()
 
 fun main() {
     println(tripleQuotedString)
