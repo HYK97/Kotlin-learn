6026dfbf0ccf91ced5735840137207ea2eaa1354
HYKim <ddh963963@naver.com>
2023-09-04 23:51:05 +0900

    feat : Extension functions

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -2,7 +2,7 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
     /* TODO */
 }
 
-fun test(date1: MyDate, date2: MyDate) {
+fun test(date1: MyDate, date2:  MyDate) {
     // this code should compile:
     println(date1 < date2)
 }
